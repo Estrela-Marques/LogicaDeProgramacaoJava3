98ba0bf424324ce233441798a6afbee86d01a951
Estrela <estelapfarma@gmail.com>
2022-09-22 18:44:05 -0300

    Adicionando capitulo 5 e 6

---
+++ CursoLogicaCapitulo06/src/ExercicioTarefasDoDia.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class ExercicioTarefasDoDia {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		String[] tarefas = new String[5];
+		
+		for(int i = 0; i < tarefas.length; i++) {
+			System.out.println("Tarefa " + i + ": ");
+			tarefas[i] = scanner.nextLine();
+		}
+		
+		System.out.println("Suas tarefas são: ");
+		for(int i = 0; i < tarefas.length; i++) {
+			System.out.println(i + ": " + tarefas[i]);
+		}
+		
+		scanner.close();
+	}
+
+}
